9d37b91a05dc2e7231f3323bd1dcfcb768980806
Student <student@lab-sec-2.cs.put.poznan.pl>
2014-11-23 13:33:38 +0100

    Strange server

--- server.c
+++ server.c
@@ -7,24 +7,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
-//#include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define PORT 1234
 #define ADDR "0.0.0.0"
 #define BACKLOG 10
 #define RESP "Pawel Chojnacki, 70950\n"
 
-void my_sigchld_handler(int sig){
+void childend(int sig){
   pid_t p;
-  int status;
 
-  while ((p=waitpid(-1, &status, WNOHANG)) != -1){
-    printf("%d died with status: %d", p, status);
-  }
+  p = wait(NULL);
+  printf("%d died\n", p);
+
+  //while (waitpid((pid_t)(-1), 0, WNOHANG) > 0) {}
 }
 
 int main(){
+  char response[] = "Pawel Chojnacki, 70950\n";
+
+ //signal(SIGCHLD, SIG_IGN);
+
   int fd = socket(PF_INET, SOCK_STREAM, 0);
   const struct sockaddr_in sock_desc = {
   	.sin_family = AF_INET,
@@ -41,20 +46,23 @@ int main(){
   assert(res==0);
 
   struct sockaddr_in client_addr;
-  socklen_t client_addr_size = sizeof(client_addr);
 
-  signal(SIGCHLD, my_sigchld_handler);
-
+  signal(SIGCHLD, childend);
   while (1){
+    socklen_t client_addr_size = sizeof(client_addr);
     int client_fd = accept(fd, (struct sockaddr *)&client_addr, &client_addr_size);
+    printf("Conn from: %s\n", inet_ntoa(client_addr.sin_addr));
     if (fork() == 0){
-	printf("%s\n", inet_ntoa(client_addr.sin_addr));
-	res = write(client_fd, RESP, sizeof(RESP));
-	assert(res == sizeof(RESP));
+    	close(fd);
+//	sleep(1);
+
+	res = write(client_fd, response, sizeof(response));
+
     	close(client_fd);
-	break;
+	//assert(res == sizeof(RESP));
+	exit(0);
     } else {
-    	close(client_fd);
+      close(client_fd);
     }
   }
 
