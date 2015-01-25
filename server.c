a4e6fbd6e3ee53966ce30964e27a4175ef19bf60
Pawel Chojnacki <pawel@chojnacki.ws>
2015-01-25 02:30:30 +0100

    splint sux

--- server.c
+++ server.c
@@ -6,14 +6,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h> 
 #include <assert.h>
-#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#endif
+
 #include <signal.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <string.h>
-#endif
 
 #define PORT 1234
 #define ADDR "0.0.0.0"
@@ -23,36 +23,47 @@ void childend(int sig){
   pid_t p;
 
   p = wait(NULL);
-  printf("%d died\n", p);
+  printf("signal code: %d pid: %d\n", sig, (int)p);
 }
 
 int exec_shell( int sock )
 {
-  close(0); 
-  close(1); 
-  close(2); 
+  if (close(0) != 0) {
+  	perror("close failed");
+  }
+  if (close(1) != 0) {
+  	perror("close failed");
+  }
+  if (close(2) != 0) {
+  	perror("close failed");
+  }
 
-  if( dup(sock) != 0 || dup(sock) != 1 || dup(sock) != 2 ) {
+  if(dup(sock) != 0 || dup(sock) != 1 || dup(sock) != 2) {
     perror("dup not duped");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
   printf("Starting shell...\n");
-  execl("/bin/bash", "/bin/bash", (char*)NULL);
-  perror("execl(3) failed");
-  exit(1);
+  if (execl("/bin/bash", "/bin/bash", NULL) != 0){
+    perror("execl(3) failed");
+  }
+  exit(EXIT_FAILURE);
 }
 
 int main(){
-  signal(SIGCHLD, childend);
+#ifndef S_SPLINT_S
 
-  int fd = socket(PF_INET, SOCK_STREAM, 0);
+  signal(SIGCHLD, childend);
+ 
+
+  int fd, on;
+  fd = socket(PF_INET, SOCK_STREAM, 0);
   const struct sockaddr_in sock_desc = {
   	.sin_family = AF_INET,
   	.sin_port = htons(PORT),
 	.sin_addr = { .s_addr = INADDR_ANY }
   };
-  int on = 1;
+  on = 1;
   setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
 
   int res = bind(fd, (const struct sockaddr *)&sock_desc, sizeof(sock_desc));
@@ -76,6 +87,7 @@ int main(){
   }
 
   close(fd);
+#endif
   return 0;
 }
 
